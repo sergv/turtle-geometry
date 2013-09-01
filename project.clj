79675101c28ced4537dd4d1039f4e21d19992d3a
Sergey Vinokurov <serg.foo@gmail.com>
2013-09-01 21:20:59 +0300

    cleared project.clj; added local jars of dependencies; bumped versions of turtle-geometry and lein-droid;

--- project.clj
+++ project.clj
@@ -9,11 +9,83 @@
       (read-string (slurp project-key-def-file))
       nil)))
 
+(defn turtle-geometry-accept-file? [^String name]
+  (or (and (re-find #"neko/init.*\.class$" name)
+           (not (re-find #"compilation" name)))
 
-(defproject turtle_geometry/turtle_geometry "0.9.0"
-  :description "Application intepreting LOGO programs in an interactive fashion.
-Intended for use with Abelson's book going under the same name."
-  :url "http://example.com/FIXME"
+      (re-find #"TurtleGraphics.*interact" name)
+      (and (re-find #"android/support/v4/app" name)
+           (not (re-find #"Notification" name))
+           (not (re-find #"NavUtils" name))
+           (not (re-find #"ShapeCompat" name)))
+
+      (re-find #"clojure/stacktrace__init" name)
+      (re-find #"clojure/template__init" name)
+      ;; (re-find #"clojure/uuid__init" name)
+      ;; (re-find #"clojure/uuid\$loading" name)
+      (re-find #"clojure/walk__init" name)
+      (re-find #"clojure/core\$bean\.class" name)
+
+      (and (re-find #".*\.class$" name)
+           (not (re-find #"clojure.*bean" name))
+
+           ;; (not (re-find #"clojure/asm" name))
+           (not (re-find #"clojure/data" name))
+           (not (re-find #"clojure/inspector" name))
+           ;; (not (re-find #"clojure/instant" name))
+           ;; (not (re-find #"clojure/java" name))
+           (not (re-find #"clojure/lang/.*XML" name))
+           (not (re-find #"clojure/main" name))
+           (not (re-find #"clojure/reflect" name))
+           (not (re-find #"clojure/repl" name))
+           (not (re-find #"clojure/.*pretty_writer" name))
+           (not (re-find #"clojure/.*pprint" name))
+           (not (re-find #"clojure/stacktrace" name))
+           (not (re-find #"clojure/template" name))
+           (not (re-find #"clojure/test" name))
+           ;; (not (re-find #"clojure/uuid" name))
+           (not (re-find #"clojure/walk" name))
+           (not (re-find #"clojure/xml" name))
+           (not (re-find #"clojure/zip" name))
+
+           ;; (not (re-find #"clojure/core\$emit_protocol" name))
+           ;; (not (re-find #"clojure/core\$emit_method_builder" name))
+           ;; (not (re-find #"clojure/core\$emit_hinted" name))
+           ;; (not (re-find #"clojure/core\$emit_impl" name))
+           ;; (not (re-find #"clojure/core\$emit_deftype" name))
+           ;; (not (re-find #"clojure/core\$emit_extend" name))
+
+           (not (re-find #"android/clojure/graphic/Transformable" name))
+           (not (re-find #"android/clojure/graphic/Transformation" name))
+           (not (re-find #"android/clojure/graphic/transformation" name))
+
+           (not (re-find #"android/support/v4" name))
+
+
+           (not (re-find #"android/annotation" name))
+           ;; (not (re-find #"android/support/v4" name))
+
+           (not (re-find #"com/sattvik" name))
+           (not (re-find #"com/android" name))
+
+           (not (re-find #"build/.*" name))
+           (not (re-find #"clojure/java/browse_ui.*" name))
+           (not (re-find #"clojure/java/javadoc.*" name))
+           (not (re-find #"clojure/java/shell.*" name))
+           (not (re-find #"clojure/lang/Repl" name))
+           (not (re-find #"clojure/lang/Script" name))
+           (not (re-find #"interact.*" name))
+           (not (re-find #"using/Active" name))
+           (not (re-find #"jsint/Listener" name))
+           (not (re-find #"jsint/Listener11" name))
+           (not (re-find #"jsint/Listener11swing" name))
+           (not (re-find #"jsint/SchemeApplet" name))
+           )))
+
+(defproject turtle-geometry/turtle-geometry "1.0.0"
+  :description "Intepreter for dialect of LOGO programs. Intended for use
+with Turtle Geometry book."
+  :url "https://github.com/sergv/turtle-geometry"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
@@ -29,106 +101,31 @@ Intended for use with Abelson's book going under the same name."
   :target-path "bin"
   :compile-path "bin/classes"
 
-  ;; :local-repo "/home/sergey/.m2/repository/"
-  :repositories [["local" "file:///home/sergey/.m2/repository/"]]
   :dependencies [[android/clojure "1.5.0"]
-                 [neko/neko "2.0.0-beta3-enhanced"]
-                 [android-utils/android-utils "0.6.0-no-drawer"]
-                 [org.clojure/math.numeric-tower "0.0.2"]
-                 ;; [org.antlr/antlr "3.5"]
-                 ]
+                 [neko/neko "2.0.0-beta3"]
+                 [android-utils/android-utils "0.7.0"]
+                 [org.clojure/math.numeric-tower "0.0.2"]]
   :profiles {:dev {:dependencies [[org.clojure/tools.nrepl "0.2.1"]]
                    :android {:aot :all-with-unused
                              :start-nrepl-server true}}
              :release {:android {:aot :all}}}
 
-  :plugins [[lein-droid "0.1.0-preview5-enhanced"]]
+  :plugins [[lein-droid "0.1.0"]]
+  :repositories [["local" "file://~/.m2/repository/"]]
   :android ~(merge
-             {:sdk-path "/home/sergey/projects/android/android-sdk-linux"
-              :gen-path "bin"
+             {:gen-path "bin"
 
               :enable-dynamic-compilation false
               :repl-device-port 10001
               :repl-local-port 10001
 
-              :accept-file-func
-              (fn [^String name]
-                (or (and (re-find #"neko/init.*\.class$" name)
-                         (not (re-find #"compilation" name)))
+              :accept-file-func turtle-geometry-accept-file?
 
-                    (re-find #"TurtleGraphics.*interact" name)
-                    (and (re-find #"android/support/v4/app" name)
-                         (not (re-find #"Notification" name))
-                         (not (re-find #"NavUtils" name))
-                         (not (re-find #"ShapeCompat" name)))
-
-                    (re-find #"clojure/stacktrace__init" name)
-                    (re-find #"clojure/template__init" name)
-                    ;; (re-find #"clojure/uuid__init" name)
-                    ;; (re-find #"clojure/uuid\$loading" name)
-                    (re-find #"clojure/walk__init" name)
-                    (re-find #"clojure/core\$bean\.class" name)
-
-                    (and (re-find #".*\.class$" name)
-                         (not (re-find #"clojure.*bean" name))
-
-                         ;; (not (re-find #"clojure/asm" name))
-                         (not (re-find #"clojure/data" name))
-                         (not (re-find #"clojure/inspector" name))
-                         ;; (not (re-find #"clojure/instant" name))
-                         ;; (not (re-find #"clojure/java" name))
-                         (not (re-find #"clojure/lang/.*XML" name))
-                         (not (re-find #"clojure/main" name))
-                         (not (re-find #"clojure/reflect" name))
-                         (not (re-find #"clojure/repl" name))
-                         (not (re-find #"clojure/.*pretty_writer" name))
-                         (not (re-find #"clojure/.*pprint" name))
-                         (not (re-find #"clojure/stacktrace" name))
-                         (not (re-find #"clojure/template" name))
-                         (not (re-find #"clojure/test" name))
-                         ;; (not (re-find #"clojure/uuid" name))
-                         (not (re-find #"clojure/walk" name))
-                         (not (re-find #"clojure/xml" name))
-                         (not (re-find #"clojure/zip" name))
-
-                         ;; (not (re-find #"clojure/core\$emit_protocol" name))
-                         ;; (not (re-find #"clojure/core\$emit_method_builder" name))
-                         ;; (not (re-find #"clojure/core\$emit_hinted" name))
-                         ;; (not (re-find #"clojure/core\$emit_impl" name))
-                         ;; (not (re-find #"clojure/core\$emit_deftype" name))
-                         ;; (not (re-find #"clojure/core\$emit_extend" name))
-
-                         (not (re-find #"android/clojure/graphic/Transformable" name))
-                         (not (re-find #"android/clojure/graphic/Transformation" name))
-                         (not (re-find #"android/clojure/graphic/transformation" name))
-
-                         (not (re-find #"android/support/v4" name))
-
-
-                         (not (re-find #"android/annotation" name))
-                         ;; (not (re-find #"android/support/v4" name))
-
-                         (not (re-find #"com/sattvik" name))
-                         (not (re-find #"com/android" name))
-
-                         (not (re-find #"build/.*" name))
-                         (not (re-find #"clojure/java/browse_ui.*" name))
-                         (not (re-find #"clojure/java/javadoc.*" name))
-                         (not (re-find #"clojure/java/shell.*" name))
-                         (not (re-find #"clojure/lang/Repl" name))
-                         (not (re-find #"clojure/lang/Script" name))
-                         (not (re-find #"interact.*" name))
-                         (not (re-find #"using/Active" name))
-                         (not (re-find #"jsint/Listener" name))
-                         (not (re-find #"jsint/Listener11" name))
-                         (not (re-find #"jsint/Listener11swing" name))
-                         (not (re-find #"jsint/SchemeApplet" name))
-                         )))
               :proguard-opts ["-ignorewarnings"]
               :proguard-conf-path "proguard.cfg"
 
-              :external-classes-paths ["/home/sergey/projects/android/android-sdk-linux/extras/android/support/v4/android-support-v4.jar"
-                                       "/mnt/disk3/projects/scheme/jscheme/lib/jscheme.jar"]
+              :external-classes-paths ["lib/android-support-v4.jar"
+                                       "lib/jscheme.jar"]
 
               ;; Uncomment this if dexer fails with OutOfMemoryException
               ;; :force-dex-optimize true
@@ -139,7 +136,6 @@ Intended for use with Abelson's book going under the same name."
               :target-version "15"
               :aot-exclude-ns ["clojure.parallel" "clojure.core.reducers"]}
              key-def-info))
-
 
 ;; Local Variables:
 ;; clojure-compile/lein-command: "LEIN_JAVA_CMD=/usr/lib/jvm/java-7-openjdk-amd64/jre/bin/java lein with-profiles %s do droid code-gen, droid compile, droid create-dex, droid apk, droid install, droid run"
